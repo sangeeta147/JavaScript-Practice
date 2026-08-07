5634197a06a5ce1bbf393af8fea56851111a38c5
Sangeeta <kundugeeta15@gmail.com>
2026-08-07 12:42:16 +0530

    Of Loop

---
+++ Loops/for-ofloop.js
@@ -0,0 +1,20 @@
+/*const arr = ['first_name','last_name','age']
+
+for(const descs of arr) {
+    console.log(descs);
+}
+
+for(const descs in arr) {
+    console.log(descs);
+}*/
+
+
+const user = {
+    name : 'Sangeeta',
+    age : 25,
+    occupation : 'Student',
+}
+
+for(const keys in user) {
+    console.log(keys);
+}
