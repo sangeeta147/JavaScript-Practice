83718d5ee1f57c6729b6234e34b04e69808050ed
Sangeeta <kundugeeta15@gmail.com>
2026-08-07 11:47:20 +0530

    Practice

--- Loops/for-inloop.js
+++ Loops/for-inloop.js
@@ -1,3 +1,15 @@
-const use = {
-    
+const user = {
+    name : 'Sangeeta',
+    age : 25,
+    occupation : 'Student',
+}
+
+for(const keys in user) {
+    console.log(keys);
+}
+
+const arr = ['Sangeeta',25,'Student']
+
+for(const index in arr) {
+    console.log(index);
 }
