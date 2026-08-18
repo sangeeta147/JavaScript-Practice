581ddd3a323cbf9f69a9965ccaf61b836e2548fc
Sangeeta <kundugeeta15@gmail.com>
2026-08-18 09:53:13 +0530

    Practice

---
+++ Assignments/Dynamically Creating and Appending an Image Element with JS/script.js
@@ -0,0 +1,9 @@
+let img = document.getElementById("img");
+function showimg(){
+    img.style.display = "block";
+img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR18RKmoSpUCsJvM1NW7AVveVIUnWjRsOXtsoTXsSx6pw&s=10";
+};
+
+function hideimg(){
+    img.style.display = "none";
+};
